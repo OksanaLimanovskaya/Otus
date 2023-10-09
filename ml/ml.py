cc2bbea817a6d6aefb86f275b02a936c022e7f4c
OksanaLimanovskaya <135599630+OksanaLimanovskaya@users.noreply.github.com>
2023-10-09 10:09:57 -0400

    Add files via upload

--- ml/ml.py
+++ ml/ml.py
@@ -1,12 +1,16 @@
 import pickle
 import numpy as np
 
-filename = 'ml/model_RF.pkl'
+filename_man = 'ml/rf_man_40.pkl'
+filename_women = 'ml/rf_women_40.pkl'
 
-with open(filename, 'rb') as file:
-    model = pickle.load(file)
+with open(filename_man, 'rb') as file:
+    model_men = pickle.load(file)
+
+with open(filename_women, 'rb') as file:
+    model_women = pickle.load(file)
     
-def load_model(ADS: int,
+def load_model_men(ADS: int,
 		ADD: int,
 		ADP: int,
 		ZDin: int,
@@ -30,8 +34,34 @@ def load_model(ADS: int,
 	 """
 	 
 	x = np.array([ADS, ADD, ADP, ZDin, ZDout, ZEL, Weight, Accommodation, Hear, Balance]).reshape(1,10)
-	res = model.predict(x)
+	res = model_men.predict(x)
 	return np.round(res[0])
-	
+
+def load_model_women(ADS: int,
+		ADD: int,
+		ADP: int,
+		ZDin: int,
+		ZDout: int,
+		ZEL: int,
+		Weight: int,
+		Accommodation: int,
+		Hear: int,
+		Balance:int) -> int:
+		
+	""" ADS - systolic pressure,
+	 ADD - dystolic pressure,
+	 ADP - difference between systolitic and dystolitic pressure,
+	 ZDin - holding your breath while inhaling in sec
+	 ZDout - breath retention on exhalation in seconds
+	 ZEL - vital capacity of the lungs in ml
+	 Weight - weight of your body, kg
+	 Accommodation - accommodation in diopters
+	 Hear - hearing acuity in belarus
+	 Balance - static balancing in seconds
+	 """
+	 
+	x = np.array([ADS, ADD, ADP, ZDin, ZDout, ZEL, Weight, Accommodation, Hear, Balance]).reshape(1,10)
+	res = model_women.predict(x)
+	return np.round(res[0])	
 		
 		
